5931c729d6ffe838a0bbbff069b7b1816ac06d14
brettrecker <47011092+brettrecker@users.noreply.github.com>
2019-02-18 17:41:37 -0600

    updated nutrionix page to display http get

--- lab3/src/app/nutritionix/nutritionix.component.ts
+++ lab3/src/app/nutritionix/nutritionix.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import { AuthService } from '../auth.service';
-import {subscribeOn} from 'rxjs/operators';
 
 @Component({
   selector: 'app-nutritionix',
@@ -10,19 +8,21 @@ import {subscribeOn} from 'rxjs/operators';
 })
 export class NutritionixComponent implements OnInit {
 
-  constructor(private http: HttpClient, private authService:AuthService) { }
-
-  posts: any
   private searchterm;
-  calories = 10;
-  sWeight = 32;
+  calories: string;
+  sWeight: string;
+  data: any;
 
+  constructor(private http: HttpClient) { }
 
-  SubmitEvent(value: any) {
+  SubmitEvent(value: any):void {
     this.searchterm = value.searchterm.toString();
-    console.log(this.searchterm)
-    this.posts = this.http.get('https://api.nutritionix.com/v1_1/search/%22' + this.searchterm +
-                    '%22?results=0:1&fields=*&appId=2972d721&appKey=d1a617bbe2e43ec825625373764e27bf');
+
+    this.http.get('https://api.nutritionix.com/v1_1/search/%22' + this.searchterm +
+                    '%22?results=0:1&fields=*&appId=2972d721&appKey=d1a617bbe2e43ec825625373764e27bf').subscribe((data) => {
+this.data = data;
+console.log(data);
+    });
   }
 
   ngOnInit() {
