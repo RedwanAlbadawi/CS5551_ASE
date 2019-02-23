441f70daf9b52e9962f8713c40b22d40cbc5d8f7
brettrecker <47011092+brettrecker@users.noreply.github.com>
2019-02-23 14:17:36 -0600

    started the http call on tab3 - http error response returned

--- lab4/src/app/tab3/tab3.page.ts
+++ lab4/src/app/tab3/tab3.page.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {}
+export class Tab3Page {
+
+  @ViewChild('places') places;
+  data: any;
+
+  constructor(private _http: HttpClient) {
+  }
+
+  seePlaces() {
+    console.log(this.places.value);
+    this._http.get('https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=iowa&inputtype=textquery&fi' +
+        'elds=photos,formatted_address,name,rating,opening_hours,geometry&key=AIzaSyCKcJGJvkHc-KkDBC-_xwZ5YgBcnf3Sxvo')
+        .subscribe((data) => {
+          this.data = data;
+          console.log(data);
+        });
+  }
+
+}
