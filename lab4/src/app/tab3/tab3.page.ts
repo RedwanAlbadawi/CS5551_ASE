e7005065c4f27bb37e0230f3eaf5f7078c2c532c
brettrecker <47011092+brettrecker@users.noreply.github.com>
2019-02-23 20:28:35 -0600

    Adding search api call to tab 3, added unit testing folder

--- lab4/src/app/tab3/tab3.page.ts
+++ lab4/src/app/tab3/tab3.page.ts
@@ -8,22 +8,23 @@ import {HttpClient} from '@angular/common/http';
 })
 export class Tab3Page {
 
-  @ViewChild('places') places;
+  @ViewChild('searchTerm') searchTerm;
   data: any;
+  data1: any;
 
-  constructor(private http: HttpClient) {
-  }
+  constructor(private http: HttpClient) {}
 
-  seePlaces() {
-    if (this.places.value !== '') {
-      console.log(this.places.value);
-      this.http.get('https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=kansas%20city&inputtype=textquery&' +
-          'fields=photos,formatted_address,name,rating,opening_hours,geometry&key=AIzaSyCKcJGJvkHc-KkDBC-_xwZ5YgBcnf3Sxvo')
-          .subscribe((data) => {
-            this.data = data;
-            console.log(data);
-          });
-    }
-  }
+  searchTerms() {
 
+    console.log(this.searchTerm.value);
+    this.http.get('https://kgsearch.googleapis.com/v1/entities:search?query=' + this.searchTerm.value +
+        '&key=AIzaSyBNOF-hKaRYwGGg781e9DEDhSv1YACMJWQ&limit=1&indent=True')
+      .subscribe((data) => {
+        this.data = data;
+      console.log(data);
+    });
+
+ }
 }
+
+
